2380611f9ed2035c7a01042d07a9a0182022958b
Javian2 <52936547+Javian2@users.noreply.github.com>
2024-01-28 19:28:42 +0100

    Add basic signals feature

--- src/app/pages/signals/container/signals.component.ts
+++ src/app/pages/signals/container/signals.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Injector,
+  OnInit,
+  Signal,
+  WritableSignal,
+  computed,
+  effect,
+  signal,
+} from '@angular/core';
 
 @Component({
   selector: 'app-signals',
   templateUrl: './signals.component.html',
-  styleUrls: ['./signals.component.scss']
+  styleUrls: ['./signals.component.scss'],
 })
+export class SignalsComponent implements OnInit {
+  constructor(private injector: Injector) {}
 
-export class SignalsComponent {
+  count: WritableSignal<number> = signal(0);
+  doubleCount: Signal<number> = computed(() => this.count() * 2);
+  notifyCount!: string;
 
+  ngOnInit(): void {
+    effect(
+      () => {
+        this.notifyCount = `Effect notification: count changed to ${this.count()}`;
+      },
+      { injector: this.injector }
+    );
+  }
+
+  setCount() {
+    this.count.set(3);
+  }
+
+  increaseCount() {
+    this.count.update(value => value + 1);
+  }
+
+  decreaseCount() {
+    this.count.update(value => value - 1);
+  }
 }
